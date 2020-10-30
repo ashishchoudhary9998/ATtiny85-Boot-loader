0e2ea8593fa217e3a9f6e468e12ea9ae91b4f117
Ashish Choudhary <40533472+ashishchoudhary9998@users.noreply.github.com>
2020-10-30 12:55:46 +0530

    Update Burn_AT85_bootloader.bat

--- Burn_AT85_bootloader.bat
+++ Burn_AT85_bootloader.bat
@@ -1,2 +1,2 @@
-"%~dp0\hardware\tools\avr/bin/avrdude" -C"%~dp0\hardware\tools\avr/etc/avrdude.conf" -v -pattiny85 -cstk500v1 -PCOM3 -b19200 -Uflash:w:"%~dp0\AT85_Bootloader.hex":i -U lfuse:w:0xe1:m -U hfuse:w:0xdd:m -U efuse:w:0xfe:m
+"%~dp0\hardware\tools\avr/bin/avrdude" -C"%~dp0\hardware\tools\avr/etc/avrdude.conf" -v -pattiny85 -cstk500v1 -PCOM5 -b19200 -Uflash:w:"%~dp0\AT85_Bootloader.hex":i -U lfuse:w:0xe1:m -U hfuse:w:0xdd:m -U efuse:w:0xfe:m
 @pause
